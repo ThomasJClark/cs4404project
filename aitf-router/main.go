9274b8b76dcbc6f13aa66957794b66f81323c839
Thomas Clark <tjclark@wpi.edu>
2015-04-27 12:17:44 -0400

    Add filter request and counter-connection structs and functions

--- aitf-router/main.go
+++ aitf-router/main.go
@@ -3,34 +3,73 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"net"
 
 	"github.com/ThomasJClark/cs4404project/aitf"
+)
+
+var (
+	victim   = net.ParseIP("8.8.8.8")
+	attacker = net.ParseIP("9.9.9.9")
+	router1  = net.ParseIP("1.2.3.4")
+	router2  = net.ParseIP("5.6.7.8")
+	router3  = net.ParseIP("9.10.11.12")
 )
 
 func main() {
 	// Sample route record
 	rr := aitf.NewRouteRecord()
 	rr.Protocol = 0x06
-	rr.AddRouter(aitf.NewRouter(net.IPv4(1, 2, 3, 4), net.IPv4(8, 8, 8, 8)))
-	rr.AddRouter(aitf.NewRouter(net.IPv4(5, 6, 7, 8), net.IPv4(8, 8, 8, 8)))
-	rr.AddRouter(aitf.NewRouter(net.IPv4(9, 10, 11, 12), net.IPv4(8, 8, 8, 8)))
+	rr.AddRouter(aitf.NewRouter(router1, victim))
+	rr.AddRouter(aitf.NewRouter(router2, victim))
+	rr.AddRouter(aitf.NewRouter(router3, victim))
 
 	// Print the route record, encode it, decode it, then print the decoded one
 	fmt.Println("rr =", rr)
 
 	b := new(bytes.Buffer)
-	if _, err := rr.WriteTo(b); err != nil {
-		log.Fatal(err)
-	}
+	rr.WriteTo(b)
 
 	fmt.Printf("bytes = % x\n", b.Bytes())
 
 	rr2 := aitf.NewRouteRecord()
-	if _, err := rr2.ReadFrom(b); err != nil {
-		log.Fatal(err)
+	rr2.ReadFrom(b)
+	fmt.Println("rr2 =", rr2)
+
+	fmt.Println("==========")
+
+	// Now the same for FilterRequest
+	req := aitf.FilterRequest{Type: aitf.FilterReq, Source: attacker, Dest: victim, Flow: rr}
+	fmt.Println("req =", req)
+
+	req.WriteTo(b)
+	fmt.Printf("bytes = % x\n", b.Bytes())
+
+	req2 := aitf.FilterRequest{}
+	req2.ReadFrom(b)
+
+	if cc := req2.ReadCounterConnection(b); cc != nil {
+		fmt.Println("cc =", cc)
+	} else {
+		fmt.Println("req2 =", req2)
 	}
 
-	fmt.Println("rr2 =", rr2)
+	fmt.Println("==========")
+
+	// And CounterConnection
+	cc := aitf.CounterConnection{Req: req, Nonce: 0xc6f5bec12a9e668b}
+	cc.Req.Type = aitf.CounterConnectionSyn
+	fmt.Println("cc =", cc)
+
+	cc.WriteTo(b)
+	fmt.Printf("bytes = % x\n", b.Bytes())
+
+	req3 := aitf.FilterRequest{}
+	req3.ReadFrom(b)
+
+	if cc := req3.ReadCounterConnection(b); cc != nil {
+		fmt.Println("cc2 =", cc)
+	} else {
+		fmt.Println("req3 =", req3)
+	}
 }
---
+++ aitf/filter_request.go
@@ -0,0 +1,116 @@
+package aitf
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+)
+
+/*
+MessageType specifies which of the five messages involved in the filter
+request process this is.
+*/
+type MessageType uint8
+
+const (
+	/*FilterReq is sent by a "victim" host to the nearest router to initiate a
+	filter request, and later to an "attacker" host.*/
+	FilterReq MessageType = iota
+
+	/*CounterConnectionSyn is sent by the victim's router to another router to
+	request a filter*/
+	CounterConnectionSyn
+
+	/*CounterConnectionSynAck is sent by the other router to the victim's router
+	to acknowledge a filter request*/
+	CounterConnectionSynAck
+
+	/*CounterConnectionAck is sent by the vicitm's router to the other router to
+	confirm the origin of the filter request.*/
+	CounterConnectionAck
+
+	/*FilterAck is sent by an "Attacker" host to a nearby gateway router to
+	signify the host's compliance with a filter request.*/
+	FilterAck
+)
+
+/*FilterRequest contains the information passed around by a victim, routers,
+and an attacker during the process of a filter request.*/
+type FilterRequest struct {
+	Type   MessageType
+	Source net.IP /*The alleged attacker*/
+	Dest   net.IP /*The alleged victim*/
+	Flow   RouteRecord
+}
+
+/*
+WriteTo writes a filter request in its binary format into w
+*/
+func (req *FilterRequest) WriteTo(w io.Writer) (n int64, err error) {
+	binary.Write(w, binary.BigEndian, req.Type)
+	binary.Write(w, binary.BigEndian, req.Source.To4())
+	binary.Write(w, binary.BigEndian, req.Dest.To4())
+	req.Flow.WriteTo(w)
+
+	return 0, nil
+}
+
+/*
+ReadFrom reads a filter request from its binary encoding in r
+*/
+func (req *FilterRequest) ReadFrom(r io.Reader) (n int64, err error) {
+	n = 0
+
+	if err = binary.Read(r, binary.BigEndian, &req.Type); err != nil {
+		return
+	}
+
+	var addresses [8]byte
+	if err = binary.Read(r, binary.BigEndian, &addresses); err != nil {
+		return
+	}
+
+	req.Source = net.IP(addresses[:4])
+	req.Dest = net.IP(addresses[4:])
+
+	return req.Flow.ReadFrom(r)
+}
+
+/*
+ReadCounterConnection checks weather a filter request is part of a
+counter-connection handshake, and reads and returns a full CounterConnection
+struct if it is.
+
+Otherwise, ReadCounterConnection returns nil.
+*/
+func (req *FilterRequest) ReadCounterConnection(r io.Reader) *CounterConnection {
+	switch req.Type {
+	case CounterConnectionSyn, CounterConnectionSynAck, CounterConnectionAck:
+		cc := new(CounterConnection)
+		cc.Req = *req
+		binary.Read(r, binary.BigEndian, cc.Nonce)
+		return cc
+	default:
+		return nil
+	}
+}
+
+/*A CounterConnection contains an entire filter request as well as a nonce.
+This is used in part of a three-way handshake between two routers to confirm
+the address of the router forwarding a filter request.*/
+type CounterConnection struct {
+	Req   FilterRequest
+	Nonce uint64
+}
+
+/*
+WriteTo writes a CounterConnection message in its raw binary format to w.  This
+is the same as writing a filter request, but with a message type indicating
+that it is a counter-connection, and an additional nonce at the end.
+*/
+func (cc *CounterConnection) WriteTo(w io.Writer) (n int64, err error) {
+	cc.Req.WriteTo(w)
+	binary.Write(w, binary.BigEndian, cc.Nonce)
+
+	return 0, nil
+}
