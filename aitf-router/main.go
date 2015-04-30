d07a86a27fe313677ccb3c8396b90e96e18a01ba
Thomas Clark <tjclark@wpi.edu>
2015-04-30 06:09:02 -0400

    Fix aitf-router
    Instead of the NF_REPEAT thing it was doing before as a test, aitf-router
    now adds a route record if there isn't one, or appends itself to the route
    record if there is.

--- aitf-router/main.go
+++ aitf-router/main.go
@@ -46,13 +46,14 @@ func main() {
 	/*Listen for any packets being forwarded by this router and create/update the
 	route record shim layer in each of them.*/
 	for packet := range nfq.GetPackets() {
+		log.Println(packet.Packet)
 		var ipLayer *layers.IPv4
 
 		/* Get the IPv4 layer, or ignore it if it doesn't exist. */
 		if layer := packet.Packet.Layer(layers.LayerTypeIPv4); layer != nil {
 			ipLayer = layer.(*layers.IPv4)
 		} else {
-			packet.SetResult(netfilter.NF_ACCEPT, nil)
+			packet.SetVerdict(netfilter.NF_ACCEPT)
 			continue
 		}
 
@@ -64,27 +65,24 @@ func main() {
 			continue
 		}
 
-		var verdict netfilter.Verdict
-
 		if routerecord.Shimmed(ipLayer) {
-			/*If the IP layer already has a shim, remove it and forward the packet.*/
-			log.Println("Got AITF shimmed packet from", ipLayer.SrcIP)
-			routerecord.Unshim(ipLayer)
-			verdict = netfilter.NF_ACCEPT
+			log.Println("Got AITF shimmed packet from", ipLayer.SrcIP, "for", ipLayer.DstIP)
 		} else {
-			/*If the IP layer does not have a shim. add one and repeat the packet.*/
-			log.Println("Got", ipLayer.Protocol, "packet from", ipLayer.SrcIP)
-			routerecord.Shim(ipLayer, routerecord.NewRouter(localIP, ipLayer.DstIP))
-			verdict = netfilter.NF_REPEAT
+			log.Println("Got", ipLayer.Protocol, "packet from", ipLayer.SrcIP, "for", ipLayer.DstIP)
 		}
+
+		/*Shim up the packet. One of the assumptions made is that each route knows
+		which hosts support AITF. All hosts in the test scenerios do, so there's
+		never a need for a router to remove the shim layer.*/
+		routerecord.Shim(ipLayer, routerecord.NewRouter(localIP, ipLayer.DstIP))
 
 		/*Serialize the IP packet. Assuming this is successful, accept it.*/
 		b, err := routerecord.Serialize(ipLayer)
 		if err != nil {
 			log.Println(err)
-			packet.SetResult(netfilter.NF_DROP, nil)
+			packet.SetVerdict(netfilter.NF_DROP)
 		} else {
-			packet.SetResult(verdict, b)
+			packet.SetResult(netfilter.NF_ACCEPT, b)
 		}
 	}
 }
